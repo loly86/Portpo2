dbca804e14c48af2150c49fcc8c35b66b0943b4b
loly86 <chunjaymin@gmail.com>
2020-12-27 15:47:09 +0900

    first commit

---
+++ pages/test.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card>
+    <v-card-title>
+      hello???
+    </v-card-title>
+
+    <router-link to="test">test</router-link>
+    <router-link to="/">home</router-link>
+  </v-card>
+</template>
